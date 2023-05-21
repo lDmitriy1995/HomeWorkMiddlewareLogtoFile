589907c5b3ff80f93aa2a148a14ff522104591d1
Admin <Admin@DESKTOP-QQR48TL>
2023-05-21 19:13:04 +0600

    Добавьте файлы проекта.

---
+++ HomeWorkMiddlewareLogtoFile/Models/ExceptionLoggingMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+public class ExceptionLoggingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionLoggingMiddleware> _logger;
+
+    public ExceptionLoggingMiddleware(RequestDelegate next, ILogger<ExceptionLoggingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            LogExceptionToFile(ex);
+            throw;
+        }
+    }
+
+    private void LogExceptionToFile(Exception ex)
+    {
+        string logFilePath = "error.log";
+        string errorMessage = $"[{DateTime.Now}] An exception occurred: {ex.Message}";
+
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(logFilePath, append: true))
+            {
+                writer.WriteLine(errorMessage);
+            }
+        }
+        catch (Exception)
+        {
+            // Если не удалось записать ошибку в файл, то игнорируем и продолжаем выполнение
+            _logger.LogError("Failed to log exception to file.");
+        }
+    }
+}
---
+++ HomeWorkMiddlewareLogtoFile/Models/MiddlewareExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+public static class MiddlewareExtensions
+{
+    public static IApplicationBuilder UseExceptionLoggingMiddleware(this IApplicationBuilder app)
+    {
+        return app.UseMiddleware<ExceptionLoggingMiddleware>();
+    }
+}
+
+public class Startup
+{
+    // ...
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+    {
+        // ...
+
+        app.UseExceptionLoggingMiddleware();
+
+        // ...
+    }
+}
